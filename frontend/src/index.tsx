344d905a384c71cff3cdb9ec6f9c614b878b21b9
Angga Samaludi <32694929+Asamaludi26@users.noreply.github.com>
2025-12-27 03:29:41 +0700

    config css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RootApp from "./App";
+import "../index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
